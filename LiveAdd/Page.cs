df0b3f6d94f1104ee0dbcdb78679cd97dae3ef9b
jacob <jmpecx@outlook.com>
2023-02-17 16:19:11 +0800

    chore: init

---
+++ LiveAdd/Page.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Playwright;
+
+namespace LiveAdd;
+
+public static class Page
+{
+    public static async Task Dodo(this IBrowserContext @context, char c)
+    {
+        var page = await context.NewPageAsync();
+        await page.GotoAsync("https://account.live.com/AddAssocId?ru=&cru=&fl=");
+        for (var i = 0; i < 10; i++)
+        {
+            for (var b = 'a'; b < 'z' + 1; b++)
+            {
+                var txt = "" + (char)c + i.ToString() + b;
+                Console.WriteLine("try:" + txt);
+                var count = await page.Locator("#AssociatedIdLive").CountAsync();
+                if (count == 0)
+                {
+                    await page.ReloadAsync();
+                    Console.WriteLine("error:reload");
+                }
+
+                await page.Locator("#AssociatedIdLive").FillAsync(txt);
+                await page.Locator("#SubmitYes").ClickAsync();
+                await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+            }
+        }
+    }
+}
---
+++ LiveAdd/Program.cs
@@ -0,0 +1,21 @@
+﻿using LiveAdd;
+using Microsoft.Playwright;
+
+using var playwright = await Playwright.CreateAsync();
+var chromium = playwright.Chromium;
+var context = await chromium.LaunchPersistentContextAsync(@"e:\playwright\", new()
+{
+    Headless = false
+});
+
+var page = await context.NewPageAsync();
+await page.GotoAsync("https://account.live.com/AddAssocId?ru=&cru=&fl=");
+// todo: finding.
+Console.ReadLine();
+// 然后继续
+for (var c = 'u'; c < 'z' + 1; c++)
+{
+    context.Dodo(c);
+}
+
+Console.ReadLine();
